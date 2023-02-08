46ab3eac1100347fb1b7af507b7146ee9da95914
TEJA <39546736+saiteja1290@users.noreply.github.com>
2023-02-09 00:51:56 +0530

    Create bttemplate.c

---
+++ bttemplate.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    /* data */
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+struct Node*buildtree(struct Node*root){
+    
+    int x;
+    struct Node*newnode = (struct Node*)malloc(sizeof(struct Node));
+    printf("Enter data >>\n");
+    scanf("%d",&x);
+    newnode -> data = x;
+    if(x == -1){
+        return NULL;
+    }
+    
+    printf("ENter the left data for %d node\n",x);
+    newnode -> left = buildtree(newnode->left);
+    printf("ENter the right data for %d node\n",x);
+    newnode -> right = buildtree(newnode -> right);
+
+    return newnode;
+
+}
+
+void main(){
+
+    struct Node* root = NULL;
+    root = buildtree(root);
+
+}
